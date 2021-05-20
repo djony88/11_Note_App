cf15e5add4f1793fb99fbaf34e40f1b0c28d3d82
Nikola <88djony@gmail.com>
2021-05-20 13:27:29 -0700

    setup express app

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+// Set up dependencies
+
 const express = requre("express");
 const fs = require("fs");
 const path = require("path");
 const database = require("./db/db");
 
+// Set up Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({
+    extended: true}));
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "notes.html"));
+})
+
+// Set up rout for GET nad POST functions
 
