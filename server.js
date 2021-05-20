61c35447b05aa5bfa3fc6e5c917978c0ee28ae0b
Nikola <88djony@gmail.com>
2021-05-20 14:38:53 -0700

    something I changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.use(express.urlencoded({
-    extended: true}));
+    extended: true }));
 
 app.use(express.json());
 
